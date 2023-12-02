d965b2c931cd6e286351e959ffa388fd1be21b57
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-12-02 12:13:28 +0000

    Solving day 1

--- src/test/java/info/jab/aoc/day1/Day1Test.java
+++ src/test/java/info/jab/aoc/day1/Day1Test.java
@@ -40,9 +40,38 @@ class Day1Test {
         Day1 day1 = new Day1();
         var result = day1.getPart2Result(fileName);
 
-        //29, 83, 13, 24, 42, 14, and 76
-
         //Then
         then(result).isEqualTo(281);
+    }
+
+    @Test
+    void shouldReplace() {
+
+        //Given
+
+        Day1 day1 = new Day1();
+        var result = day1.replacePatterns
+            .andThen(day1.toCharList)
+            .andThen(day1.toListOfDigits)
+            .andThen(day1.applyRules)
+            .apply("7pqrstsixteen");
+
+        //When
+
+        //Then
+        then(result).isEqualTo(76);
+    }
+
+    @Test
+    void should_solve_day1_part2() {
+        //Given
+        String fileName = "day1/day1-input.txt";
+
+        //When
+        Day1 day1 = new Day1();
+        var result = day1.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(52840);
     }
 }
