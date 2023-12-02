6b04537b2811464fa51d8364d73562a0b867f3bb
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-12-02 15:12:04 +0000

    Solving Day 2

--- src/main/java/info/jab/aoc/day2/Day2.java
+++ src/main/java/info/jab/aoc/day2/Day2.java
@@ -13,10 +13,9 @@ public class Day2 implements Day<Long> {
     @Override
     public Long getPart1Result(String fileName) {
         return Utils.readFileToList(fileName).stream()
-            //.peek(System.out::println)
             .filter(game -> {
-                //only 12 red cubes, 13 green cubes, and 14 blue cubes
 
+                //only 12 red cubes, 13 green cubes, and 14 blue cubes
                 var maxRed = 12;
                 var maxGreen = 13;
                 var maxBlue = 14;
@@ -41,7 +40,6 @@ public class Day2 implements Day<Long> {
                             }
                         }
                         if(tuple[1].contains("blue")) {
-                            //blue += Integer.valueOf(tuple[0]);
                             var currentBlue = Integer.valueOf(tuple[0]);
                             if(currentBlue > maxBlue) {
                                 return false;
@@ -52,7 +50,7 @@ public class Day2 implements Day<Long> {
 
                 return true;
             })
-            .peek(System.out::println)
+            //.peek(System.out::println)
             .map(game -> {
                 var parts = game.split(":");
                 var parts2 = parts[0].split(" ");
@@ -63,7 +61,45 @@ public class Day2 implements Day<Long> {
 
     @Override
     public Long getPart2Result(String fileName) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return Utils.readFileToList(fileName).stream()
+            .map(game -> {
+
+                var red = 0L;
+                var green = 0L;
+                var blue = 0L;
+
+                var parts = game.split(":");
+                var parts2 = parts[1].split(";");
+
+                for (String cubeSet : parts2) {
+                    var cubeSetParts = cubeSet.split(",");
+                    for (String item : cubeSetParts) {
+                        var tuple = item.trim().split(" ");
+                        if(tuple[1].contains("red")) {
+                            var currentRed = Integer.valueOf(tuple[0]);
+                            if(currentRed > red) {
+                                red = currentRed;
+                            }
+                        }
+                        if(tuple[1].contains("green")) {
+                            var currentGreen = Integer.valueOf(tuple[0]);
+                            if(currentGreen > green) {
+                                green = currentGreen;
+                            }
+                        }
+                        if(tuple[1].contains("blue")) {
+                            var currentBlue = Integer.valueOf(tuple[0]);
+                            if(currentBlue > blue) {
+                                blue = currentBlue;
+                            }
+                        }
+                    }
+                }
+
+                return red * green * blue;
+            })
+            .reduce(0L, (a, b) -> a + b);
+
     }
 
 }
--- src/test/java/info/jab/aoc/day2/Day2Test.java
+++ src/test/java/info/jab/aoc/day2/Day2Test.java
@@ -30,4 +30,30 @@ class Day2Test {
         //Then
         then(result).isEqualTo(2683);
     }
+
+    @Test
+    void should_solve_day2_part2_with_sample() {
+        //Given
+        String fileName = "day2/input-sample.txt";
+
+        //When
+        Day2 day2 = new Day2();
+        var result = day2.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(2286);
+    }
+
+    @Test
+    void should_solve_day2_part2() {
+        //Given
+        String fileName = "day2/input.txt";
+
+        //When
+        Day2 day2 = new Day2();
+        var result = day2.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(49710);
+    }
 }
