3ba5dd72aff0550a664232cb3241394ba2a7c782
Juan Antonio Breña Moral <bren@juanantonio.info>
2023-12-04 20:49:29 +0000

    Adding solution for Day4

--- src/main/java/info/jab/aoc/day4/Day4.java
+++ src/main/java/info/jab/aoc/day4/Day4.java
@@ -1,6 +1,13 @@
 package info.jab.aoc.day4;
 
 import info.jab.aoc.Day;
+import info.jab.aoc.Utils;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Solution for AOC 2023, Day 4
@@ -9,15 +16,50 @@ import info.jab.aoc.Day;
  */
 class Day4 implements Day<Long> {
 
+    // @formatter:off
+
+    Function<String, Integer> getMatches = param -> {
+        String[] numbers = param.split(":")[1].split("\\|");
+
+        List<String> winningNumbers = Arrays.stream(numbers[0].split(" "))
+            .filter(c -> !c.isBlank() && !c.equals(" "))
+            .collect(Collectors.toList());
+
+        int matches = Arrays.stream(numbers[1].split(" "))
+            .filter(base -> winningNumbers.contains(base))
+            .mapToInt(s -> 1)
+            .sum();
+
+        return matches;
+    };
+
     @Override
     public Long getPart1Result(String fileName) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPart1Result'");
+        var result = Utils.readFileToList(fileName).stream()
+            .map(getMatches)
+            .map(c -> ((1 << c) / 2))
+            .reduce(0, (a, b) -> a + b);
+        return Long.valueOf(result);
+    }
+
+    // @formatter:on
+
+    //Recursive approach
+    private long iterate(Map<Integer, Integer> cache, List<String> lines, int start, int end) {
+        long counter = 0L;
+        for (int i = start; i < end; i++) {
+            int match = cache.computeIfAbsent(i, k -> getMatches.apply(lines.get(k)));
+            if (match > 0) {
+                counter += iterate(cache, lines, i + 1, i + 1 + match);
+            }
+            counter++;
+        }
+        return counter;
     }
 
     @Override
     public Long getPart2Result(String fileName) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getPart2Result'");
+        var lines = Utils.readFileToList(fileName);
+        return iterate(new HashMap<>(), lines, 0, lines.size());
     }
 }
--- src/test/java/info/jab/aoc/day4/Day4Test.java
+++ src/test/java/info/jab/aoc/day4/Day4Test.java
@@ -16,6 +16,45 @@ public class Day4Test {
         var result = day.getPart1Result(fileName);
 
         //Then
-        then(result).isEqualTo(8);
+        then(result).isEqualTo(13);
+    }
+
+    @Test
+    void should_solve_day4_part1() {
+        //Given
+        String fileName = "day4/input.txt";
+
+        //When
+        Day4 day = new Day4();
+        var result = day.getPart1Result(fileName);
+
+        //Then
+        then(result).isEqualTo(18619);
+    }
+
+    @Test
+    void should_solve_day4_part2_with_sample() {
+        //Given
+        String fileName = "day4/input-sample2.txt";
+
+        //When
+        Day4 day = new Day4();
+        var result = day.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(30);
+    }
+
+    @Test
+    void should_solve_day4_part2() {
+        //Given
+        String fileName = "day4/input.txt";
+
+        //When
+        Day4 day = new Day4();
+        var result = day.getPart2Result(fileName);
+
+        //Then
+        then(result).isEqualTo(8063216);
     }
 }
